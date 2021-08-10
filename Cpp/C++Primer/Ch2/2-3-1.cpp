8a120387884014ffef8fa611cc9c718cbad7d942
FredH <1515435570@qq.com>
2021-08-11 04:06:30 +0800

    more notes on references

--- Cpp/C++Primer/Ch2/2-3-1.cpp
+++ Cpp/C++Primer/Ch2/2-3-1.cpp
@@ -5,6 +5,7 @@ int main(){
     //reference is an alias of the object refered to, it's binded to the object;
     //whenever the reference is given a new value the referenced object changes;
     //visiting the reference is exactly same as visiting the variable;
+    //reference is not an object, so it can't be copied.
     int i{19}, &ri = i;
     i = 5;
     ri = 10;
