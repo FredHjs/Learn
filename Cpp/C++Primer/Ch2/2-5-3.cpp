585501fda918a733a85a96d5e4d629f4f2c89a6c
FredH <1515435570@qq.com>
2021-08-12 05:08:28 +0800

    auto, decltype

--- Cpp/C++Primer/Ch2/2-5-3.cpp
+++ Cpp/C++Primer/Ch2/2-5-3.cpp
@@ -1,14 +1,21 @@
 # include <iostream>
 
 int main(){
-    int a{3}, b{4};
+    int a{3}, b{0};
+    auto x = 4;     //auto type variables must be initialized;
 
     decltype(a) c = a;      //equivalent to: int c = a;
 
     //equivalent to: int &d = a;
     decltype((a)) d = a;        //decltype((variable)) gives the reference to the type of the variable
-
-    ++c;    //4
-    ++d;    //4
-    std::cout << c << " " << d <<std::endl;
+    decltype(b = x) e = a;  //assignment yeilds reference type
+   
+    ++c;    //c == 4
+    std::cout << c << std::endl;
+    
+    ++d;    //a == 4
+    std::cout << d << std::endl;
+    
+    ++e;    //a == 5
+    std::cout << e << std::endl;
 }
