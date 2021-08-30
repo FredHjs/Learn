cdac442a581cf8500da2ac7b3c42445a380a586b
FredH <1515435570@qq.com>
2021-08-30 22:53:30 +0800

    functions

--- Cpp/C++Primer/Ch6/6-1.cpp
+++ Cpp/C++Primer/Ch6/6-1.cpp
@@ -9,14 +9,29 @@ int factorial(int value){
     return result;
 }
 
+unsigned int abs_val(int v){
+    if (v < 0){
+        v = -v;
+    }
+
+    return static_cast<unsigned> (v);
+}
+
 int main(){
     unsigned int v{1};
+    int v2{0};
     std::cout << "enter a positive integer: ";
 
     std::cin >> v;
 
     std::cout << std::endl;
-    std::cout << v << "! = " << factorial(v); 
+    std::cout << v << "! = " << factorial(v) << std::endl;
+
+    std::cout << "now enter an arbitrary integer: ";
+    std::cin >> v2;
+    std::cout << std::endl;
+
+    std::cout << "the absolute value of " << v2 << " is " << abs_val(v2);
 
 }
 
