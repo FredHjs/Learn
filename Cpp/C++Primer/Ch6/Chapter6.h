ac3242435d570535be8cb37f978d6feb561ac161
FredH <1515435570@qq.com>
2021-08-30 23:12:03 +0800

    Separate compilation

--- Cpp/C++Primer/Ch6/Chapter6.h
+++ Cpp/C++Primer/Ch6/Chapter6.h
@@ -1,7 +1,10 @@
+//the pre-processor clauses prevents the header file from being compiled multiple times
 #ifndef CHAPTER6_H
 #define CHAPTER6_H
 
+//the functions used is defined in this header file
 int factorial(int value);
+
 unsigned int abs_val(int v);
 
 #endif
