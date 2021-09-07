9917198812d7814fbcfdd48940bd347facd09bba
FredH <1515435570@qq.com>
2021-09-07 22:27:53 +0800

    add read and print operations to Person class

--- Cpp/C++Primer/Ch7/Person.h
+++ Cpp/C++Primer/Ch7/Person.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 struct Person{
     string Name;
@@ -8,3 +9,14 @@ struct Person{
     string get_name() const {return Name;}
     string get_address() const {return Address;}
 };
+
+istream& read(Person person, istream& is){
+    getline(is, person.Name);
+    getline(is, person.Address);
+    return is;
+}
+
+ostream& print(Person person, ostream& os){
+    os << person.Name << ": " << person.Address;
+    return os;
+}
