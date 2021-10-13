5bdc632fcbbf0673957b93a4dac41e4154c205cd
FredH <1515435570@qq.com>
2021-10-13 22:58:48 +0800

    swap operation

--- Cpp/C++Primer/Ch13/HasPtr_ValueLike.h
+++ Cpp/C++Primer/Ch13/HasPtr_ValueLike.h
@@ -1,15 +1,22 @@
 #include <string>
+#include <iostream>
 
 class HasPtr{
+    friend void swap(HasPtr& lhs, HasPtr& rhs);
     public:
-    
-        HasPtr(const std::string& s = std::string()): ps(new std::string(s)), i(0){}
+        HasPtr(const std::string& s = std::string(), const int i = 0): ps(new std::string(s)), i(i){}
 
         //A copy constructor must have a parameter which is the reference of its own type, and other parameters
         //must have default values.
         HasPtr(const HasPtr&);
+        
         HasPtr& operator=(const HasPtr& rhs);
+        bool operator<(const HasPtr& rhs) const {return *ps < *(rhs.ps);}
+        
         ~HasPtr();
+        const std::string get_s() const {return *ps;} 
+        const int get_i() const {return i;}
+        void inline swap(HasPtr&);
     private:
         std::string* ps;
         int i;
@@ -29,4 +36,15 @@ HasPtr& HasPtr::operator=(const HasPtr& rhs){
 HasPtr::~HasPtr(){
     delete ps;
     ps = nullptr;
+}
+
+void inline HasPtr::swap(HasPtr& rhs){
+    using std::swap;
+    std::cout << "calling swap(HasPtr&, HasPtr&)" << std::endl;
+    swap(ps, rhs.ps);
+    swap(i, rhs.i);
+}
+
+void swap(HasPtr& lhs, HasPtr& rhs){
+    lhs.swap(rhs);
 }
