06c63112f1f4ae26206d047476744b810ebb9e0f
FredH <1515435570@qq.com>
2021-10-15 23:27:51 +0800

    copy control exercise: Messages and Folders

--- Cpp/C++Primer/Ch13/Folder.h
+++ Cpp/C++Primer/Ch13/Folder.h
@@ -1,12 +1,14 @@
 #ifndef FOLDER_H
 #define FOLDER_H
 #include <set>
+#include <initializer_list>
 #include "Message.h"
 using namespace std;
 
 class Folder{
     public:
         Folder() = default;
+        Folder(initializer_list<Message*> messages);
         Folder& add_msg(Message*);
         Folder& remove_msg(Message*);
 
