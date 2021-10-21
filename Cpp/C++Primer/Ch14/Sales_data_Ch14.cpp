b753b32651f9d3519022aa94a5dc197c8f3bf3e6
FredH <1515435570@qq.com>
2021-10-21 23:49:47 +0800

    Add overloaded IO operators

---
+++ Cpp/C++Primer/Ch14/Sales_data_Ch14.cpp
@@ -0,0 +1,41 @@
+#include "Sales_data_Ch14.h"
+
+inline Sales_data::Sales_data(std::istream &is): Sales_data(){
+    is >> ISBN >> num_sold >> unit_price;
+    revenue = num_sold * unit_price;
+    std::cout << "ctor with with an istream& argument is called" << std::endl;
+}
+
+Sales_data& Sales_data::combine(const Sales_data& rhs){
+    if (this->ISBN != rhs.ISBN){
+        throw std::runtime_error("not the same book!");
+    }
+
+    this->num_sold += rhs.num_sold;
+    this->revenue += rhs.revenue;
+
+    return *this;
+}
+
+std::ostream& print(std::ostream& os, Sales_data sd){
+    os << sd.ISBN + " " << sd.num_sold + " " << sd.unit_price << " " << sd.revenue;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os,const Sales_data& sd){
+    os << sd.ISBN + " " << sd.num_sold + " " << sd.unit_price << " " << sd.revenue;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Sales_data& sd){
+    is >> sd.ISBN >> sd.num_sold >> sd.unit_price;
+
+    if (is.good()){
+        //need to make sure sd.unit_price is stored successfully
+        sd.revenue = sd.num_sold * sd.unit_price;
+    }else{
+        //the operator>> is responsible for reverting from a possible error
+        sd = Sales_data();
+        throw std::invalid_argument("Please enter in the format of \"ISBN num_sold unit_price\"!");
+    }
+}
---
+++ Cpp/C++Primer/Ch14/Sales_data_Ch14.h
@@ -0,0 +1,44 @@
+#ifndef SALES_DATA_CH14_H
+#define SALES_DATA_CH14_H
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+class Sales_data{
+    friend std::ostream& print(std::ostream& os, Sales_data sd);
+    friend std::ostream& operator<<(std::ostream&, const Sales_data&);
+    friend std::istream& operator>>(std::istream&, Sales_data&);
+
+    public:
+        Sales_data(const std::string &s, unsigned n, double p): 
+            ISBN(s), num_sold(n), revenue(p*n){
+                std::cout << "ctor with 3 arguments is called" << std::endl;
+            }
+        
+        Sales_data(): Sales_data("", 0, 0.0){
+            std::cout << "the default ctor is called" << std::endl;
+        }
+
+        Sales_data(const std::string &s): Sales_data(s, 0, 0.0){
+            std::cout << "ctor with a string argument is called" << std::endl;
+        }
+
+        Sales_data(std::istream &is);
+
+        std::string isbn() const {return ISBN;}
+
+        Sales_data& combine(const Sales_data& rhs);
+
+        inline double avg_price() const {return revenue/num_sold;}
+
+    private:
+        std::string ISBN;
+
+        int num_sold{0};
+
+        double unit_price{0.0};
+
+        double revenue{0.0};
+};
+
+#endif
