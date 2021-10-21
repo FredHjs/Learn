ebb883a86154b505749d175d0c0263fb67affefa
FredH <1515435570@qq.com>
2021-10-21 23:51:02 +0800

    Add overloaded == and != operators to StrBlob

---
+++ Cpp/C++Primer/Ch12/StrBlob.cpp
@@ -0,0 +1,58 @@
+#include "StrBlob.h"
+
+StrBlob& StrBlob::operator=(const StrBlob& rhs){
+    auto new_data = make_shared<vector<string>>(*(rhs.data));
+
+    this->data.reset(new_data.get());
+
+    return *this;
+}
+
+bool StrBlob::check(vector<string>::size_type index, const string& msg) const {
+    if (index >= data->size()){
+        throw out_of_range(msg);
+    }
+
+    return true;
+}
+void StrBlob::pop_back(){
+    if (check(0, "no data stored!")){
+        data->pop_back();
+    }
+}
+
+shared_ptr<vector<string>> StrBlobPtr::check(const string& msg, vector<string>::size_type index) const{
+    auto shared_ptr = wp.lock();
+    if (shared_ptr){
+        if (shared_ptr->size() > index){
+            return shared_ptr;
+        }else{
+            throw out_of_range(msg);
+        }
+    }else{
+        throw runtime_error("object pointer not set to an object");
+    }
+}
+
+string& StrBlobPtr::deref() const{
+    auto shared_ptr = check("index out of range", this->curr);
+    return (*shared_ptr)[this->curr];
+}
+
+StrBlobPtr& StrBlobPtr::incr(){
+    auto shared_ptr = check("increament out of range", this->curr);
+    ++this->curr;
+    return *this;
+}
+
+StrBlobPtr StrBlob::begin() {return StrBlobPtr(*this, 0);}
+
+StrBlobPtr StrBlob::end() {auto ret = StrBlobPtr(*this, this->data->size()); return ret;}
+
+bool operator==(const StrBlob& lhs, const StrBlob& rhs){
+    return *lhs.data == *rhs.data;
+}
+
+bool operator!=(const StrBlob& lhs, const StrBlob& rhs){
+    return !(*lhs.data == *rhs.data);
+}
--- Cpp/C++Primer/Ch12/StrBlob.h
+++ Cpp/C++Primer/Ch12/StrBlob.h
@@ -9,6 +9,9 @@ class StrBlobPtr;
 
 class StrBlob{
     friend class StrBlobPtr;
+    friend bool operator==(const StrBlob&, const StrBlob&);
+    friend bool operator!=(const StrBlob& lhs, const StrBlob& rhs);
+    
     public:
         StrBlob(): data(make_shared<vector<string>>()){}
 
@@ -48,65 +51,25 @@ class StrBlob{
         string& get_back() const {if (check(0, "no data stored!")) return data->back();}
 };
 
-StrBlob& StrBlob::operator=(const StrBlob& rhs){
-    auto new_data = make_shared<vector<string>>(*(rhs.data));
-
-    this->data.reset(new_data.get());
-
-    return *this;
-}
-
-bool StrBlob::check(vector<string>::size_type index, const string& msg) const {
-    if (index >= data->size()){
-        throw out_of_range(msg);
-    }
-
-    return true;
-}
-void StrBlob::pop_back(){
-    if (check(0, "no data stored!")){
-        data->pop_back();
-    }
-}
-
 class StrBlobPtr{
     public:
         StrBlobPtr():curr(0){}
+
         StrBlobPtr(StrBlob& strBlob, size_t sz = 0): wp(strBlob.data), curr(sz){}
+
         string& deref() const;
+
         StrBlobPtr& incr();
+        
         bool operator!=(const StrBlobPtr& rhs) {return this->curr != rhs.curr;}
+
     private:
         shared_ptr<vector<string>> check(const string& msg, vector<string>::size_type index) const;
+
         vector<string>::size_type curr;
+
         weak_ptr<vector<string>> wp;
 };
 
-shared_ptr<vector<string>> StrBlobPtr::check(const string& msg, vector<string>::size_type index) const{
-    auto shared_ptr = wp.lock();
-    if (shared_ptr){
-        if (shared_ptr->size() > index){
-            return shared_ptr;
-        }else{
-            throw out_of_range(msg);
-        }
-    }else{
-        throw runtime_error("object pointer not set to an object");
-    }
-}
 
-string& StrBlobPtr::deref() const{
-    auto shared_ptr = check("index out of range", this->curr);
-    return (*shared_ptr)[this->curr];
-}
-
-StrBlobPtr& StrBlobPtr::incr(){
-    auto shared_ptr = check("increament out of range", this->curr);
-    ++this->curr;
-    return *this;
-}
-
-StrBlobPtr StrBlob::begin() {return StrBlobPtr(*this, 0);}
-
-StrBlobPtr StrBlob::end() {auto ret = StrBlobPtr(*this, this->data->size()); return ret;}
 #endif
