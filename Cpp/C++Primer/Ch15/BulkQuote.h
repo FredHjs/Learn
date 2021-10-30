6ff1936af9519caa4d89954ab0b2b45ffac77082
FredH <1515435570@qq.com>
2021-10-30 22:16:53 +0800

    inherited ctor

--- Cpp/C++Primer/Ch15/BulkQuote.h
+++ Cpp/C++Primer/Ch15/BulkQuote.h
@@ -12,8 +12,6 @@ class BulkQuote : public DiscQuote{
             According to the arg list*/
         using DiscQuote::DiscQuote;
 
-        BulkQuote(const BulkQuote&);
-
         BulkQuote& operator=(const BulkQuote&);
         
         double net_price(std::size_t) const override;
@@ -25,8 +23,6 @@ double BulkQuote::net_price(std::size_t num_sold) const{
     if (num_sold >= min_qty) return unit_price * (1 - discount) * num_sold;
     else return unit_price * num_sold;
 }
-
-BulkQuote::BulkQuote(const BulkQuote& rhs): DiscQuote(rhs) {std::cout << "BulkQuote copy ctor" << std::endl;}
 
 BulkQuote& BulkQuote::operator=(const BulkQuote& rhs){
     std::cout << "BulkQuote operator=()" << std::endl;
