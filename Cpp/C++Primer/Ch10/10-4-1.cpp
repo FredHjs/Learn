59b9490163c1c51a115cfe4a2e441a5d3a181382
FredH <1515435570@qq.com>
2021-11-02 22:44:17 +0800

    insert iterators

--- Cpp/C++Primer/Ch10/10-4-1.cpp
+++ Cpp/C++Primer/Ch10/10-4-1.cpp
@@ -10,7 +10,7 @@ int main(){
 
     copy(vi.begin(), vi.end(), front_inserter(lst1));   //9 8 7 6 5 4 3 2 1 
     copy(vi.begin(), vi.end(), back_inserter(lst2));        //1 2 3 4 5 6 7 8 9 
-    copy(vi.begin(), vi.end(), inserter(lst1, lst3.begin()));       //1 2 3 4 5 6 7 8 9
+    copy(vi.begin(), vi.end(), inserter(lst3, lst3.begin()));       //1 2 3 4 5 6 7 8 9
     
     for(auto& i : lst1){
         cout << i << " ";
