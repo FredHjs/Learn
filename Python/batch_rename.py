0a0ab84ed2236f448063293263e7850506cc2e94
FredH <1515435570@qq.com>
2021-11-15 12:22:29 +0800

    update

--- Python/batch_rename.py
+++ Python/batch_rename.py
@@ -1,14 +1,15 @@
 import os
 
-path = 'D:\\Projects\\uploads\\2020\\06'
+root = 'D:\\Projects\\uploads\\2021'
 
-file_list = os.listdir(path)
+for d in os.listdir(root):
+    path = root + '\\' + d
+    file_list = os.listdir(path)
+    for file_name in file_list:
+        old_name = path + '\\' + file_name
+        suffix = file_name.split('.')[-1]
+        name = file_name.rsplit('_', 2)[0]
 
-for file_name in file_list:
-    old_name = path + '\\' + file_name
-    suffix = file_name.split('.')[-1]
-    name = file_name.rsplit('_', 3)[0]
+        new_name = path + '\\' + name + '.' + suffix
 
-    new_name = path + '\\' + name + '.' + suffix
-
-    os.rename(old_name, new_name)
+        os.rename(old_name, new_name)
