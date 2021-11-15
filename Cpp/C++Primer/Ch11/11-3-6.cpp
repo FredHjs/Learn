f222c80a737d576504989ee159c556d053befc7b
FredH <1515435570@qq.com>
2021-11-15 23:44:35 +0800

    update for random choice of mapped words

--- Cpp/C++Primer/Ch11/11-3-6.cpp
+++ Cpp/C++Primer/Ch11/11-3-6.cpp
@@ -4,9 +4,11 @@
 #include <map>
 #include <string>
 #include <stdexcept>
+#include <random>
+#include <vector>
 using namespace std;
 
-map<string, string> build_map(ifstream& map_file);
+map<string, vector<string>> build_map(ifstream& map_file);
 const string& transform(const string& word, map<string, string>& map);
 void word_transform(ifstream& map_file, ifstream& text_file);
 
@@ -15,27 +17,36 @@ int main(){
     word_transform(map_file, text_file);
 }
 
-map<string, string> build_map(ifstream& map_file){
-    map<string, string> transform_map;
-    string word, mapped_to;
+map<string, vector<string>> build_map(ifstream& map_file){
+    map<string, vector<string>> transform_map;
+    string word, mapped_to, mapped_word;
     while (map_file >> word && getline(map_file, mapped_to)){
-        if (mapped_to.size() > 1)
-            transform_map[word] = mapped_to.substr(1);
-        else throw invalid_argument("No mapping rules found for " + word);
+        if (mapped_to.size() > 1){
+            istringstream is(mapped_to);
+            
+            while(is >> mapped_word){
+                transform_map[word].push_back(mapped_word);
+            }
+        }else{
+            throw invalid_argument("No mapping rules found for " + word);
+        } 
     }
 
     return transform_map;
 }
 
-const string& transform(const string& word, map<string, string>& map){
-    if (map.find(word) != map.end())
-        return map.find(word) -> second;
-    else
+const string& transform(const string& word, map<string, vector<string>>& map){
+    static default_random_engine e;
+    
+    if (map.find(word) != map.end()){
+        uniform_int_distribution<int> u(0,(map.find(word)->second).size() - 1);
+        return (map.find(word) -> second)[u(e)];
+    }else
         return word;
 }
 
 void word_transform(ifstream& map_file, ifstream& text_file){
-    map<string, string> map = build_map(map_file);
+    map<string, vector<string>> map = build_map(map_file);
     string line, word;
 
     while (getline(text_file, line)){
