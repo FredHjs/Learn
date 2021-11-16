85053a7d5b5ff5d4bd445243429b379f0e938bf9
FredH <1515435570@qq.com>
2021-11-17 00:04:31 +0800

    format IO

--- Cpp/C++Primer/Ch17/17-5-1.cpp
+++ Cpp/C++Primer/Ch17/17-5-1.cpp
@@ -4,11 +4,9 @@ using std::cout;
 using std::endl;
 
 int main(){
-    cout << std::setw(12);
     cout << 1.17 << std::setw(12) << endl;
-    cout << 1.24 << std::setw(12) << endl;
-    cout << std::setprecision(3) << std::setw(12) <<  1.555 << endl;
-    cout << std::hexfloat << std::setprecision(3) << std::setw(12) << 3.14159265368 << defaultfloat endl;
-    cout << std::oct
-
+    cout << std::setfill('#') << 1.24 << std::setw(12) << endl;
+    cout << std::setfill(' ') << std::setprecision(3) << std::setw(12) <<  1.555 << endl;
+    cout << std::hexfloat << std::setprecision(3) << std::setw(12) << 3.14159265368 << std::defaultfloat << endl;
+    cout << std::oct << std::setw(12) << 9 << std::dec << endl;
 }
