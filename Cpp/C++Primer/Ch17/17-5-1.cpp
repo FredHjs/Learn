4c9d21aaa2b0d91fa7f20b052198cc2778a932f0
FredH <1515435570@qq.com>
2021-11-16 23:59:33 +0800

    format IO

---
+++ Cpp/C++Primer/Ch17/17-5-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::endl;
+
+int main(){
+    cout << std::setw(12);
+    cout << 1.17 << std::setw(12) << endl;
+    cout << 1.24 << std::setw(12) << endl;
+    cout << std::setprecision(3) << std::setw(12) <<  1.555 << endl;
+    cout << std::hexfloat << std::setprecision(3) << std::setw(12) << 3.14159265368 << defaultfloat endl;
+    cout << std::oct
+
+}
