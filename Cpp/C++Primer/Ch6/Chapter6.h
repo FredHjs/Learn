4f370f6ba77ab0084fee4daf5d4249aea4e3e736
FredH <1515435570@qq.com>
2021-11-29 23:57:54 +0800

    re-organize folders

--- Cpp/C++Primer/Ch6/Chapter6.h
+++
@@ -1,9 +0,0 @@
-//the pre-processor clauses prevents the header file from being compiled multiple times
-#ifndef CHAPTER6_H
-#define CHAPTER6_H
-
-//the functions used is declared in this header file
-int factorial(int value);
-unsigned int abs_val(int v);
-
-#endif
--- Cpp/C++Primer/Ch7/7-1-3.cpp
+++
@@ -1,8 +0,0 @@
-#include "Person.h"
-using namespace std;
-int main(){
-    Person person, &p = person;
-    read(p, cin);
-    print(p, cout);
-}
-
